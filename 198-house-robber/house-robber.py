7d302748cb49d8314cf390266f342866d861f9c5
Nimish Srivastav <nimish.srivastav@gmail.com>
2025-06-27 17:44:58 -0700

    Time: 2 ms (8.96%) | Memory: 18 MB (13.08%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,21 +1,40 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        # if not nums:
+        #     return 0
 
-        if len(nums) == 1:
-            return nums[0]
+        # if len(nums) == 1:
+        #     return nums[0]
 
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
+        # if len(nums) == 2:
+        #     return max(nums[0], nums[1])
 
-        dp = [0] * len(nums)
+        # dp = [0] * len(nums)
 
-        dp[0] = nums[0]
+        # dp[0] = nums[0]
 
-        dp[1] = max(nums[0], nums[1])
+        # dp[1] = max(nums[0], nums[1])
 
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
-        return dp[-1]
+        # return dp[-1]
+        memo = {}
+
+        def max_amount(arr):
+            if not arr:
+                return 0
+            
+            if len(arr) == 1:
+                return arr[0]
+
+            inputs = tuple(arr)
+
+            if inputs in memo:
+                return memo[inputs]
+
+            memo[inputs] = max(arr[0] + max_amount(arr[2:]), max_amount(arr[1:]))
+
+            return memo[inputs]
+
+        return max_amount(nums)
