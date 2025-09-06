757bdc8b06fabf8719f41449090435c428cbe934
Nimish Srivastav <nimish.srivastav@gmail.com>
2025-09-06 15:37:41 -0700

    Time: 4 ms (77.11%) | Memory: 43.2 MB (76.61%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabets = new int[26];
+
+        for (int i = 0; i < s.length(); i++) alphabets[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabets[t.charAt(i) - 'a']--;
+        for (int i : alphabets) if (i != 0) return false;
+
+        return true;
+    }
+}
