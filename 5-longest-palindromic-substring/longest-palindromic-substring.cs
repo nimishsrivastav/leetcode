58503989da52d1a7379010ad67be07af47293c3b
Nimish Srivastav <nimish.srivastav@gmail.com>
2025-10-04 14:53:40 -0700

    Time: 22 ms (79.80%) | Memory: 41.3 MB (76.20%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        if (string.IsNullOrEmpty(s)) return "";
+        if (s.Length == 1) return s;
+
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < s.Length; i++) {
+            int odd = ExpandAroundCenter(s, i, i);
+            int even = ExpandAroundCenter(s, i, i + 1);
+
+            int len = Math.Max(odd, even);
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+
+        return s.Substring(start, maxLen);
+    }
+
+    private int ExpandAroundCenter(string s, int left, int right) {
+        while (left >= 0 && right < s.Length && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
