e0402659dbd0ebf879c94805f6d06cbeaf21dd2a
Nimish Srivastav <nimish.srivastav@gmail.com>
2025-10-04 15:02:45 -0700

    Time: 21 ms (83.87%) | Memory: 29.2 MB (57.16%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Reverse(int x) {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int remainder = x % 10;
+            int temp = result * 10 + remainder;
+
+            if ((temp - remainder) / 10 != result) return 0;
+
+            result = temp;
+            x /= 10;
+        }
+
+        return result;
+    }
+}
