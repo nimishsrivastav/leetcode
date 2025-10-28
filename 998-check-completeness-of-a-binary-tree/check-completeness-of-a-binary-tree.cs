51e9fb135fd6c816874afa8179767ceb486a1554
Nimish Srivastav <nimish.srivastav@gmail.com>
2025-10-28 13:00:26 -0700

    Time: 0 ms (100.00%) | Memory: 44.3 MB (89.90%) - LeetSync

---
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsCompleteTree(TreeNode root) {
+        if (root == null) return true;
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        bool foundNull = false;
+
+        while (queue.Count > 0) {
+            TreeNode current = queue.Dequeue();
+
+            if (current == null) {
+                foundNull = true;
+            }
+            else {
+                if (foundNull) return false;
+
+                queue.Enqueue(current.left);
+                queue.Enqueue(current.right);
+            }
+        }
+
+        return true;
+    }
+}
