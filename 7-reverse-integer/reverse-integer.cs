a27593936cba2616f6100055e13af148f8205951
Nimish Srivastav <nimish.srivastav@gmail.com>
2025-11-03 11:37:07 -0800

    Time: 23 ms (75.44%) | Memory: 28.9 MB (80.54%) - LeetSync

--- 7-reverse-integer/reverse-integer.cs
+++ 7-reverse-integer/reverse-integer.cs
@@ -2,15 +2,22 @@ public class Solution {
     public int Reverse(int x) {
         int result = 0;
 
-        while (x != 0)
-        {
-            int remainder = x % 10;
-            int temp = result * 10 + remainder;
+        // process all digits
+        while (x != 0) {
+            int digit = x % 10;
 
-            if ((temp - remainder) / 10 != result) return 0;
+            x = x / 10;
 
-            result = temp;
-            x /= 10;
+            // positive overflow
+            if (result > int.MaxValue / 10 || (result == int.MaxValue / 10 && digit > 7))
+                return 0;
+            
+            // negative overflow
+            if (result < int.MinValue / 10 || (result == int.MinValue / 10 && digit < -8))
+                return 0;
+            
+            // build the reversed number
+            result = result * 10 + digit;
         }
 
         return result;
