ac320b35fff4326c36753c831c3ab694769322b5
Nimish Srivastav <nimish.srivastav@gmail.com>
2025-11-06 22:49:56 -0800

    Time: 43 ms (33.66%) | Memory: 69.8 MB (71.44%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        if (k == 0) return false;
+
+        var window = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (window.Contains(nums[i]))
+                return true;
+
+            window.Add(nums[i]);
+
+            if (window.Count > k)
+                window.Remove(nums[i - k]);
+        }
+
+        return false;
+    }
+}
